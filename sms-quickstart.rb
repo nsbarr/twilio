0797e3952ceb0b634f5b129ace4cc648c2f68d28
nsbarrcanvas <nick@canv.as>
2013-07-06 18:12:49 -0400

    afd

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -5,8 +5,13 @@ require 'sinatra'
 enable :sessions
  
 get '/sms-quickstart' do
+  sender = params[:From]
+  friends = {
+    "+19143934990" => "Curious George",
+  }
+  name = friends[sender] || "Mobile Monkey"
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Sms "Hey Monkey. Thanks for the message!"
+    r.Sms "Hello, #{name}. Thanks for the message."
   end
   twiml.text
 end
