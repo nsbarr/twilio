1803c7635ea1204a01ff561e985096b89f08fe50
nsbarrcanvas <nick@canv.as>
2013-07-06 17:25:58 -0400

    helll

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -1,10 +1,3 @@
-require 'rubygems'
-require 'twilio-ruby'
-require 'sinatra'
- 
-get '/sms-quickstart' do
-  twiml = Twilio::TwiML::Response.new do |r|
-    r.Sms "Hey Monkey. Thanks for the message!"
-  end
-  twiml.text
+get '/' do
+  "Hello World"
 end
