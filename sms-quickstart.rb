2e40b4d8a3b81ea9f3a7fcbcd2226b8adf7edb34
nsbarrcanvas <nick@canv.as>
2013-07-06 18:40:19 -0400

    whatevs

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -5,12 +5,18 @@ require 'sinatra'
 enable :sessions
  
 get '/sms-quickstart' do
+  #sender is the phone number of whoever just texted you
+  sender = params[:From]
+  #magical sms counter
   session["counter"] ||= 0
   sms_count = session["counter"]
+  #for now, always assume we just got the request from the user
+  topic = params[:Body]
+  #logic on what to send the user
   if sms_count == 0
-    message = "Hello, thanks for the new message."
+    message = "Hi there, what would you like me to write you a poem about?"
   else
-    message = "Hello, thanks for message number #{sms_count + 1}"
+    message = "OK, I'll write a poem about #{topic}"
   end
   twiml = Twilio::TwiML::Response.new do |r|
     r.Sms message
